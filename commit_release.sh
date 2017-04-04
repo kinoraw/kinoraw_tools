a26149c20f8afdd3e10c618538aaed0034c2b985
carlos padial <surreal@asturnazari.com>
2017-04-04 21:35:29 +0200

    fix #2

--- commit_release.sh
+++ commit_release.sh
@@ -1,5 +1,7 @@
-cp $1 ~/sources/kinoraw_repo/
+git mv $1 archive/
+cp $2 ~/sources/kinoraw_repo/
 cd ~/sources/kinoraw_repo/
-git add $1
+git add $2
+git rm $1
 git commit -a -m "new kinoraw_tools release"
 git push
